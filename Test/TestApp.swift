bc449a2b05303c114f2f39754ef6b9160a02fc68
adil <amedeuyev@gmail.com>
2026-06-02 16:49:02 +0500

    Initial commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by adil on 02.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
